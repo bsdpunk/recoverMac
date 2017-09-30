a60eb5a1b26339458c7107d9529eb7775e0ce088
Dusty <bsdpunk@gmail.com>
2017-09-30 14:50:01 -0500

    NewLines

--- recover.sh
+++ recover.sh
@@ -6,10 +6,17 @@ brew install coreutils
 brew install gnu-sed
 brew install wget
 brew install grep
-cd Downloads && wget https://iterm2.com/downloads/stable/iTerm2-3_0_10.zip && unzip iTerm2-3_0_10.zip
+#cd Downloads && wget https://iterm2.com/downloads/stable/iTerm2-3_0_10.zip && unzip iTerm2-3_0_10.zip
+brew tap Caskroom/cask
+brew install Caskroom/cask/iterm2
 mkdir ~/go
 go get -d github.com/bsdpunk/beastietools
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/git/dotfiles/.vimrc ~/;
+brew install nvm
+source ~/.bash_profile
+nvm install node
+npm install -g bower grunt-cli jshint
 echo "Install XCode Full"
 echo "Install XCode Full"
+

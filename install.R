d05c9dd4b6eb4c053f5abf1ba0ff21cb37c57bcc
Dusty <bsdpunk@gmail.com>
2017-09-30 18:32:14 -0500

    R

---
+++ install.R
@@ -0,0 +1,1 @@
+install.packages(c("ggplot2","scales"), repos='https://cran.revolutionanalytics.com')
--- startRecovery.sh
+++ startRecovery.sh
@@ -22,7 +22,7 @@ xcode-select --install
 read -p "Press [Enter] key when Xcode CLI is finished installing..."
  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install gcc && brew install go --cross-compile-common  && brew install httpie;
  mkdir ~/git; cd ~/git; http https://api.github.com/users/bsdpunk/repos |python -m json.tool| grep '"name"' | awk -F: '{print $2}' | tr -d \" | tr -d , > repos; for i in $(cat repos); do git clone https://github.com/bsdpunk/$i; done  && cp ~/git/dotfiles/.bash_profile ~/;
-brew tap Caskroom/cask &&  brew install coreutils gnu-sed wget nvm Caskroom/cask/iterm2
+brew tap homebrew/science && brew tap Caskroom/cask &&  brew install coreutils gnu-sed wget nvm Caskroom/cask/iterm2 R
 mkdir ~/go
 go get -d github.com/bsdpunk/beastietools
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -37,6 +37,8 @@ sudo cp -R /Volumes/Google\ Chrome/Google\ Chrome.app/ /Applications/
 ## Keybase Download
 cd ~/Downloads; wget https://prerelease.keybase.io/Keybase.dmg
 
+## R libraries
+Rscript install.R
 echo "Install XCode Full"
 echo "Install XCode Full"
 echo "Install XCode Full"

36d519922e1f7b95d5df9134b087e7804f01bd09
Dusty <bsdpunk@gmail.com>
2017-09-30 19:44:06 -0500

    push

--- startRecovery.sh
+++ startRecovery.sh
@@ -11,7 +11,7 @@
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
+#        AUTHOR: Dusty Carver (), 
 #  ORGANIZATION: 
 #       CREATED: 09/30/2017 15:28
 #      REVISION:  ---
@@ -23,10 +23,17 @@ read -p "Press [Enter] key when Xcode CLI is finished installing..."
  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install gcc && brew install go --cross-compile-common  && brew install httpie;
  mkdir ~/git; cd ~/git; http https://api.github.com/users/bsdpunk/repos |python -m json.tool| grep '"name"' | awk -F: '{print $2}' | tr -d \" | tr -d , > repos; for i in $(cat repos); do git clone https://github.com/bsdpunk/$i; done  && cp ~/git/dotfiles/.bash_profile ~/;
 brew tap homebrew/science && brew tap Caskroom/cask &&  brew install coreutils gnu-sed wget nvm Caskroom/cask/iterm2 R
+## My go installs
 mkdir ~/go
 go get -d github.com/bsdpunk/beastietools
+go get -d github.com/bsdpunk/getFilthy
+go install github.com/bsdpunk/beastietools
+go install github.com/bsdpunk/getFilthy
+
+#vim stuff
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/git/dotfiles/.vimrc ~/;
+
 source ~/.bash_profile
 nvm install node
 npm install -g bower grunt-cli jshint
